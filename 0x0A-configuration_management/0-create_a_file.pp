276dff2a21452fde144ace1210c7e0a5f603237f
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-20 02:34:38 +0000

    task 0 -> create in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file {'/tmp/school/':
-	ensure => file,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => "I love Puppet"
-}
+# creates a file in /tmp
+
+file { '/tmp/school/' :
+  path		=> '/tmp/school/',
+  mode		=> '0744',
+  owner		=> 'www-data',
+  group		=> 'www-data',
+  content	=> "I love Puppet",
+  }
