2ec2dbc660df2de877b82f817d6d639e9a6de99e
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-20 13:16:30 +0000

    puppet file for automation

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,40 @@
+#install and congigure an nginx web server
+
+package {"nginx":
+  ensure => installed,
+}
+
+file {"/var/www/html/index.html":
+  ensure  => file;
+  content => "Hello World!",
+}
+
+file {"/var/www/html/my_404.html":
+  ensure  => file;
+  content => "Ceci n'est pas une page",
+}
+
+file {"/etc/nginx/sites-available/default":
+  ensure  => file,
+  content => "server {
+	listen 80;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	error_page 404 /my_404.html;
+
+	location /redirect_me {
+		return 301;
+	}
+
+	location /404 {
+		root /var/www/html;
+		internal;
+	}
+}",
+  require => Package["nginx"],
+}
+
+service {"nginx"
+  ensure  => running,
+  require => Package["nginx"],
+}
