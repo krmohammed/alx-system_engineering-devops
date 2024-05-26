09d7c9f7d08e7da7fefacd91c465a80678a6ad68
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-26 08:24:47 +0000

    task 1, readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pytho3
+""" uses a REST API """
+import requests
+import sys
+
+
+emp_id = sys.argv[1]
+emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+todo_url = f"{emp_url}/todos"
+
+emp_response = requests.get(emp_url)
+todo_response = requests.get(todo_url)
+
+emp = emp_response.json()
+todos = todo_response.json()
+
+emp_name = emp.get('name')
+all_tasks = len(todos)
+completed = [todo for todo in todos if todo.get('completed')]
+
+print(f"Employee {emp_name} is done with tasks({len(completed)}/{all_tasks}):")
+
+for todo in completed:
+    print(f"\t{todo.get('title')}")
